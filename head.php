bc77d2dc1a2b739f85241fa68028eeac692076c9
Dan Klotz <danferth@gmail.com>
2018-10-06 00:17:07 +0000

    serving font awesm scss and fonts from local and not a cdn now

--- head.php
+++ head.php
@@ -27,10 +27,11 @@ if($production){ $v = "3.1.2"; }else{ $v = rand(); } ?>
         
         <!-- Humans.txt-->
         <link type="text/plain" rel="author" href="/humans.txt">
-        <!-- css -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        
         <!-- You know what would be cool? some google font action! -->
         <!-- Well..... we're waiting on the fonts bruh -->
+        
+        <!-- css -->
         <link rel="stylesheet" href="/assets/build/css/site.css?ver=<?php echo $v; ?>">
 
     </head>
--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].'/head.php'; ?>
 
-<div class="page-wrap">
-  <h1>start</h1>
-  <p>This is in a page wrap</p>
+<div class="page-wrap grid-x">
+  <div class="cell small-12">
+    <h1>start</h1>
+    <p>This is in a page wrap <i class="fa fa-smile-o"></i></p>
+  </div>
 </div>
 
 <?php include $_SERVER['DOCUMENT_ROOT'].'/foot.php'; ?>
